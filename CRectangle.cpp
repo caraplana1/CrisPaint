824f7291c35c0f0c7d4cb7f9fe84549d50de3b4e
caraplana1 <cristianlandaez@gmail.com>
2024-12-07 19:56:53 -0400

    Rectangle implementation class

--- CRectangle.cpp
+++ CRectangle.cpp
@@ -1,2 +1,36 @@
 #include "pch.h"
 #include "CRectangle.h"
+
+CRectangle::CRectangle()
+{
+	this->x0 = 0;
+	this->x1 = 0;
+	this->y0 = 0;
+	this->y1 = 0;
+
+	this->type = SQUARE;
+}
+
+CRectangle::CRectangle(int x0, int x1, int y0, int y1)
+{
+	this->x0 = x0;
+	this->x1 = x1;
+	this->y0 = y0;
+	this->y1 = y1;
+
+	this->type = SQUARE;
+}
+
+void CRectangle::render(CDC* pDC)
+{
+	pDC->MoveTo(x0, y0);
+	pDC->Rectangle(x0, y0, x1, y1);
+}
+
+void CRectangle::read(CArchive& ar)
+{
+}
+
+void CRectangle::write(CArchive& ar)
+{
+}
--- CRectangle.h
+++ CRectangle.h
@@ -5,6 +5,7 @@ class CRectangle : CShape
 {
 public:
 	CRectangle();
+	CRectangle(int x0, int x1, int y0, int y1);
 	~CRectangle();
 	void render(CDC* pDC);
 	void read(CArchive& ar);
