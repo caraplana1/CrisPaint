2156726914f1339e948b13eb5a5657fec8ea5c15
caraplana1 <cristianlandaez@gmail.com>
2024-12-08 14:08:17 -0400

    Add color and know is a point is inside a shape virtual methods added

--- CShape.cpp
+++ CShape.cpp
@@ -2,6 +2,39 @@
 #include "CShape.h"
 
 
+std::vector<BYTE> CShape::ClampColor(float r, float g, float b)
+{
+    BYTE R, G, B;
+
+    // Clamp red
+    if (r > 1)
+        R = 255;
+    else if (r < 0)
+        R = 0;
+    else
+        R = r * 255;
+
+    // Clamp green
+    if (g > 1)
+        G = 255;
+    else if (g < 0)
+        G = 0;
+    else
+        G = g * 255;
+    
+    // Clamp blue
+    if (b > 1)
+        B = 255;
+    else if (b < 0)
+        B = 0;
+    else
+        B = b * 255;
+
+    std::vector<BYTE> result = {R, G, B};
+
+    return result;
+}
+
 CShape::CShape()
 {
 }
--- CShape.h
+++ CShape.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "pch.h"
 #include "CShape.h"
+#include <string>
+#include <vector>
 
 enum PRIMITIVE_ID
 {
@@ -16,7 +18,11 @@ class CShape
 {
 protected:
 	bool isReady;
+	bool isFiled;
+	CBrush brush;
+	CPen pen;
 	PRIMITIVE_ID type;
+	std::vector<BYTE> ClampColor(float r, float g, float b);
 public:
 	CShape();
 	~CShape();
@@ -24,6 +30,10 @@ public:
 	virtual void render(CDC* pDC) = 0;
 	virtual void read(CArchive &ar) = 0;
 	virtual void write(CArchive& ar) = 0;
+
+	virtual std::string ToString() = 0;
+	virtual bool IsInside(int x, int y) = 0;
+	virtual void paintShape(float r, float g, float b) = 0;
 	bool IsReady();
 
 };
